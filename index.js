cfc0f61c08ada66331f93e3794d8a91f9fe0afc6
Juthapong <s6206021621091@email.kmutnb.ac.th>
2023-10-02 22:46:18 +0700

    update

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'sqlite',
   storage: './Database/SQPlayers.sqlite',
 });
-
+const sequelize2 = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: './Database/SQTeams.sqlite',
+});
 // define the Book model
 const Player = sequelize.define('player', {
   id: {
@@ -104,6 +108,102 @@ app.delete('/players/:id', (req, res) => {
         res.status(404).send('Player not found');
       } else {
         player
+          .destroy()
+          .then(() => {
+            res.send({});
+          })
+          .catch((err) => {
+            res.status(500).send(err);
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+const Team = sequelize2.define('team', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  teamname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  
+});
+
+// Create the teams table if it doesn't exist
+sequelize2.sync();
+
+// Route to get all teams
+app.get('/teams', (req, res) => {
+  Team.findAll()
+    .then((teams) => {
+      res.json(teams);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// Route to get a team by id
+app.get('/teams/:id', (req, res) => {
+  Team.findByPk(req.params.id)
+    .then((team) => {
+      if (!team) {
+        res.status(404).send('Team not found');
+      } else {
+        res.json(team);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// Route to create a new team
+app.post('/teams', (req, res) => {
+  Team.create(req.body)
+    .then((team) => {
+      res.send(team);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// Route to update a team
+app.put('/teams/:id', (req, res) => {
+  Team.findByPk(req.params.id)
+    .then((team) => {
+      if (!team) {
+        res.status(404).send('Team not found');
+      } else {
+        team
+          .update(req.body)
+          .then(() => {
+            res.send(team);
+          })
+          .catch((err) => {
+            res.status(500).send(err);
+          });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+// Route to delete a team
+app.delete('/teams/:id', (req, res) => {
+  Team.findByPk(req.params.id)
+    .then((team) => {
+      if (!team) {
+        res.status(404).send('Team not found');
+      } else {
+        team
           .destroy()
           .then(() => {
             res.send({});
